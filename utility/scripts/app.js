466e87c62bcbb3dc7b98a5edf1f299e593f535a6
Amaan Uddin <mohdamaanuddin100@gmail.com>
2023-01-04 15:45:57 +0300

    seperate styles & responsive

--- utility/scripts/app.js
+++ utility/scripts/app.js
@@ -1,9 +1,24 @@
 
 const header = document.querySelector("header")
-
-
+const hamburger = document.querySelector(".hamburger")
+const overlay = document.querySelector(".overlay")
 console.log("active")
 
 window.addEventListener("scroll",() => {
     header.classList.toggle("scrolled",window.pageYOffset > 0)
 })
+
+
+
+// ------------------------------------hamburger open & close------------------------------------
+
+hamburger.addEventListener("click",() => {
+    document.body.classList.toggle("open")
+    document.body.classList.toggle("stop-scrolling")
+})
+
+overlay.addEventListener("click",() => {
+    document.body.classList.toggle("open")
+    document.body.classList.toggle("stop-scrolling")
+})
+
